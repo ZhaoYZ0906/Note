e3001c6cd9ebdb24f7607c419c60b4809428ec1d
zyz <1044196262@qq.com>
2019-03-28 21:02:46 +0800

    定期备份

---
+++ leetcode/1的个数/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a = 111101;
+            var solution = new Solution().HammingWeight(a);
+        }
+    }
+
+    public class Solution
+    {
+        //n为2进制数题目有问题
+        public int HammingWeight(uint n)
+        {
+            int result=0;
+            while (n > 0) {
+                if (n%2==1)
+                {
+                    result++;                   
+                }
+                n = n / 2;
+            }
+            return result;
+        }
+    }
+}
